931c7062a8d07ec4a935d50ea13d2227d3da9ba4
jamescrosbie <jamescrosbie@hotmail.co.uk>
2023-03-04 17:37:40 +0000

    first testing workflow

--- R/my_functions.R
+++ R/my_functions.R
@@ -2,10 +2,10 @@
 library(dplyr)
 
 
-my_add <- function(x,y){
-    return(x+y)
+my_add <- function(x, y) {
+    return(x + y)
 }
 
-my_sub <- function(x,y){
-    return(x-y)
+my_sub <- function(x, y) {
+    return(x - y)
 }
