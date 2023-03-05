76684db5e289157863b894fc5e10f39d69418961
jamescrosbie <jamescrosbie@hotmail.co.uk>
2023-03-05 19:08:11 +0000

    new tests added to test pull request in usethis

--- tests/testthat/test_my_functions.R
+++ tests/testthat/test_my_functions.R
@@ -15,5 +15,10 @@ testthat::test_that("sub function",{
     expect_equal(my_sub(10,2), 8)
 })
 
+testthat::test_that("sub function 2",{
+    expect_equal(my_sub(100,2), 98)
+    expect_equal(my_sub(10.1,2), 8.1)
+})
+
 # as this is not a package run
 # testthat::test_dir("tests/testhat") from the project root folder
